60720fd946bb40d946b06da6947018e0a0615a5f
서동우 <sdw1211@gmail.com>
2018-08-21 14:04:57 +0900

    1차 형태 커밋

---
+++ readme.md
@@ -0,0 +1,1 @@
+* boot spring boot 책 정리 및 예제 소스 코딩
---
+++ src/main/java/com/otawang/bootspringboot/BootSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.otawang.bootspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootSpringBootApplication.class, args);
+    }
+}
