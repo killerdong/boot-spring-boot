d8fa4361c94c3611bd0ab8ea217b2611d63e085f
서동우 <sdw1211@gmail.com>
2018-08-21 14:54:19 +0900

    챕터1 추가

---
+++ src/main/java/com/otawang/bootspringboot/api/HelloController.java
@@ -0,0 +1,14 @@
+package com.otawang.bootspringboot.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/greeting")
+    public String greeting() {
+        return "Hello, Boot spring Boot.";
+
+    }
+}
